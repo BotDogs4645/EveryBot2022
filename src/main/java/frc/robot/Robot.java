2690e6649e9efbe779edc1c7688b5cc67df304f5
Sharon Mendoza <smendoza37@cps.edu>
2022-02-20 18:57:26 -0600

    2022 EveryBot

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,265 @@
+/*
+  2022 everybot code
+  written by carson graf 
+  don't email me, @ me on discord
+*/
+
+/*
+  This is catastrophically poorly written code for the sake of being easy to follow
+  If you know what the word "refactor" means, you should refactor this code
+*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+public class Robot extends TimedRobot {
+  
+  //Definitions for the hardware. 
+  //update port definitions 
+  CANSparkMax upperLeft = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax lowerLeft = new CANSparkMax(2, MotorType.kBrushless);
+  CANSparkMax upperRight = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax lowerRight = new CANSparkMax(4, MotorType.kBrushless);
+
+  CANSparkMax arm = new CANSparkMax(5, MotorType.kBrushless);
+  VictorSPX intake = new VictorSPX(6);
+
+  final MotorControllerGroup leftMotors = new MotorControllerGroup(upperLeft, lowerLeft);
+  final MotorControllerGroup rightMotors = new MotorControllerGroup(upperRight, lowerRight); 
+
+  DifferentialDrive differentialDriveSub = new DifferentialDrive(leftMotors, rightMotors);
+
+  double leftSpeed;
+  double rightSpeed;
+
+  XboxController xbox = new XboxController(0);
+  JoystickButton button1 = new JoystickButton(xbox, 1);
+  JoystickButton button2 = new JoystickButton(xbox, 2);
+  JoystickButton button3 = new JoystickButton(xbox, 3);
+  JoystickButton button4 = new JoystickButton(xbox, 4);
+  JoystickButton button5 = new JoystickButton(xbox, 5);
+
+  //Constants for controlling the arm. consider tuning these for your particular robot
+  final double armHoldUp = 0.08;
+  final double armHoldDown = 0.13;
+  final double armTravel = 0.5;
+
+  final double armTimeUp = 0.5;
+  final double armTimeDown = 0.35;
+
+  //Varibles needed for the code
+  boolean armUp = true; //Arm initialized to up because that's how it would start a match
+  boolean burstMode = false;
+  double lastBurstTime = 0;
+
+  double autoStart = 0;
+  boolean goForAuto = false;
+
+  //limelight 
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-console");
+
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry tv = table.getEntry("tv");
+  NetworkTableEntry ta = table.getEntry("ta");
+  NetworkTableEntry ty = table.getEntry("ty");
+
+  //Limelight tracking variables
+  double MIN_ROT_SPEED = .15;
+  double ROT_MULTIPLIER = -0.05;
+  double LIMELIGHT_HEIGHT = 3.25; // distance from ground in inches
+  double GOAL_HEIGHT = 57.5; //in inches
+  
+
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    //Configure motors to turn correct direction. You may have to invert some of your motors
+    leftMotors.setInverted(true);
+    rightMotors.setInverted(false);
+  
+
+    //BurnFlash: For the SPARK MAX to remember its new configuration through a power-cycle, the settings must be saved using. 
+    upperLeft.burnFlash();
+    lowerLeft.burnFlash();
+    upperRight.burnFlash();
+    lowerRight.burnFlash();
+    
+    arm.setInverted(false);
+    //Brake Mode will effectively short all motor wires together
+    arm.setIdleMode(IdleMode.kBrake);
+    arm.burnFlash();
+
+    //add a thing on the dashboard to turn off auto if needed
+    SmartDashboard.putBoolean("Go For Auto", false);
+    goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
+  }
+
+  @Override
+  public void autonomousInit() {
+    //get a time for auton start to do events based on time later
+    autoStart = Timer.getFPGATimestamp();
+    //check dashboard icon to ensure good to do auto
+    goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    //arm control code. same as in teleop
+    if(armUp) {
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp) {
+        arm.set(armTravel);
+      }
+      else {
+        arm.set(armHoldUp);
+      }
+    }
+    else {
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown) {
+        arm.set(-armTravel);
+      }
+      else{
+        arm.set(-armHoldUp);
+      }
+    }
+    
+    //get time since start of auto
+    double autoTimeElapsed = Timer.getFPGATimestamp() - autoStart;
+    leftSpeed = -0.3;
+    rightSpeed = -0.3;
+    
+
+    if(goForAuto){
+      //series of timed events making up the flow of auto
+      if(autoTimeElapsed < 3) {
+        //spit out the ball for three seconds
+        intake.set(ControlMode.PercentOutput, -1);
+      } else if(autoTimeElapsed < 6) {
+        //stop spitting out the ball and drive backwards *slowly* for three seconds
+        intake.set(ControlMode.PercentOutput, 0);
+        differentialDriveSub.tankDrive(leftSpeed, rightSpeed);
+
+      } else {
+        //do nothing for the rest of auto
+        stop();
+      }
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() { 
+    leftSpeed = xbox.getLeftY();
+    rightSpeed = xbox.getRightY();
+    
+    differentialDriveSub.tankDrive(leftSpeed, rightSpeed);
+  
+    //Intake controls
+    if(xbox.getRawButton(5)) {
+      intake.set(VictorSPXControlMode.PercentOutput, 1);
+    }
+    else if(xbox.getRawButton(7)) {
+      intake.set(VictorSPXControlMode.PercentOutput, -1);
+    }
+    else {
+      intake.set(VictorSPXControlMode.PercentOutput, 0);
+    }
+
+    //Arm Controls
+    if(armUp){
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
+        arm.set(armTravel);
+      }
+      else{
+        arm.set(armHoldUp);
+      }
+    }
+    else{
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
+        arm.set(-armTravel);
+      }
+      else{
+        arm.set(-armHoldDown);
+      }
+    }
+  
+    if(xbox.getRawButtonPressed(6) && !armUp){
+      lastBurstTime = Timer.getFPGATimestamp();
+      armUp = true;
+    }
+    else if(xbox.getRawButtonPressed(8) && armUp){
+      lastBurstTime = Timer.getFPGATimestamp();
+      armUp = false;
+    }  
+
+    //limelight mode
+    if(xbox.getRawButtonPressed(3)){
+      trackObject();
+    }
+
+  }
+
+  public void stop(){
+    leftSpeed = 0;
+    rightSpeed = 0;
+    differentialDriveSub.tankDrive(leftSpeed, rightSpeed);
+    arm.set(0);
+    intake.set(ControlMode.PercentOutput, 0);
+
+  }
+
+  public void trackObject() {
+    double xOffset = -tx.getDouble(0.0);
+    SmartDashboard.putNumber("xOffset", xOffset);
+    double finalRot = 0.0;
+    if (xOffset < .25) { //0.25 represents 1/4 of a degree as measured by the limelight, this prevents the robot from overshooting its turn
+      finalRot = ROT_MULTIPLIER * xOffset + MIN_ROT_SPEED;
+    }
+    else if (xOffset > .25) {   // dampens the rotation at the end while turning
+      finalRot = ROT_MULTIPLIER * xOffset - MIN_ROT_SPEED;
+    }
+    differentialDriveSub.tankDrive(finalRot, -finalRot);
+  }
+
+  public double getDistance() {
+    double yOffset = ty.getDouble(0.0);
+    double radians = Math.toRadians(yOffset);
+    double distance;
+    
+    distance = ((LIMELIGHT_HEIGHT - GOAL_HEIGHT)/Math.tan(radians))/12;
+    SmartDashboard.putNumber("Distance to Target", distance);
+    return distance;
+  }
+
+  @Override
+  public void disabledInit() {
+    //On disable turn off everything
+    //done to solve issue with motors "remembering" previous setpoints after reenable
+    stop();
+  }
+}
