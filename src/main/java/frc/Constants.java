49da1caef7b98ef854d47ad2920c2d8626e9ade4
Yamilett <yestrada.re@gmail.com>
2022-03-11 01:09:29 -0600

    added a constants class

---
+++ src/main/java/frc/Constants.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc;
+
+/** Add your docs here. */
+public final class Constants {
+    public static final class DriveConstants {
+        public static final int DRIVE_CONTROLLER = 0; // defined on port 0
+        public static final double SPEED = 0.5; // defined on port 0
+    }
+
+    public static final class ButtonConstants {
+        // INTAKE
+        public static final int INTAKE_ABSORB = 1;
+        public static final int INTAKE_UNABSORB = 2;
+
+        // ARM
+        public static final int ARM_UP = 3;
+        public static final int ARM_DOWN = 4;
+
+
+    }
+}
