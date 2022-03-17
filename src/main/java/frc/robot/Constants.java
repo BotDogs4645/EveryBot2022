af21531bf9dd5705d19441a17c8b5f3e236787da
Yamilett Estrada-Reyes <93739159+Aidan747@users.noreply.github.com>
2022-03-17 10:45:53 -0500

    accounted for spring

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class DriveConstants {
+        public static final int DRIVE_CONTROLLER = 0; // defined on port 0
+        public static final int BUTTON_CONTROLLER = 1;
+        public static final double SPEED = 0.3; // defined on port 0
+    }
+
+    public static final class ButtonConstants {
+        // INTAKE
+        public static final int INTAKE_ABSORB = 3;
+        public static final int INTAKE_UNABSORB = 2;
+
+        // ARM
+        public static final int ARM_UP = 4;
+        public static final int ARM_DOWN = 1;
+
+
+    }
+}
